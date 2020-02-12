36b08010743067873348ff10fa53f76cb31000c8
johnkirtley <kirtleyj16@gmail.com>
2020-02-12 15:39:48 -0800

    localStorage allowing data to persist

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
-export const useLocalStorage = (key, initialValue) => {
+export const useLocalStorage = (key, initialValue = '') => {
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
 
-        return item ? JSON.parse(item) : initialValue;
+        return item ? JSON.parse(item) : setValue;
     });
 
     const setValue = (value) => {
@@ -12,6 +12,6 @@ export const useLocalStorage = (key, initialValue) => {
         window.localStorage.setItem(key, JSON.stringify(value))
     };
 
-    return [storedValue, setStoredValue, setValue];
+    return [storedValue, setValue];
 
 }
